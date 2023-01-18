a326606aa717a9d5de27b66cf692f963fbd0016d
artem <ip.bakhmatov@mail.ru>
2023-01-18 20:40:25 +0300

    Работаем с JSX-препроцессором, ограничения в нем

---
+++ src/App.js
@@ -0,0 +1,19 @@
+//import React from 'react'; писать не нужно везде(кроме в index.js) для 18 версии
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;

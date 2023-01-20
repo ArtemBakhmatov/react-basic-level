e947eb933a8643c85414e3fc0c271d783f309bd3
artem <ip.bakhmatov@mail.ru>
2023-01-20 20:09:59 +0300

    Практика свойств на проекте(props)

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -7,50 +7,26 @@ import EmployeesAddForm from '../employees-add-form/employees-add-form';
 import './app.css';
 
 function App() {
-  return (
-    <div className="app">
-        <AppInfo />
+	const data = [     // массив с данными // представляем что они приходят с сервера
+		{name: 'John C.', salary: 800, increase: false},
+		{name: 'Alex M.', salary: 3000, increase: true},
+		{name: 'Carl V.', salary: 5000, increase: false},
+	]; 
 
-        <div className="search-panel">
-            <SearchPanel/>
-            <AppFilter/>
-        </div>
-        
-        <EmployeesList/>
-        <EmployeesAddForm/>
-    </div>
-  );
+	return (
+		<div className="app">
+			<AppInfo />
+
+			<div className="search-panel">
+				<SearchPanel/>
+				<AppFilter/>
+			</div>
+			
+			<EmployeesList data={data}/>
+			<EmployeesAddForm/>
+		</div>
+	);
 }
 
 export default App;
 
-////////// Cвойства компонентов //////////////
-/* function WhoAmI (props) { 
-	return (
-		<div>
-			<h1>My name is {props.name}, surname - {props.surname}</h1>
-			<a href={props.link}>My Profile</a>
-		</div>
-	)
-}
-
-function WhoAmI ({name, surname, link}) {  // через деструктирезацию
-	return (
-		<div>
-			<h1>My name is {name()}, surname - {surname}</h1>
-			<a href={link}>My Profile</a>
-		</div>
-	)
-}
-
-// Пропсы (props) -> свойтво по русски
-
-function App() {
-	return (
-		<div className="App">
-			<WhoAmI name={() => {return 'John'}} surname="Smith" link="facebook.com"/>
-			<WhoAmI name="Alex" surname="Shepard" link="vk.com"/>
-		</div>
-	)
-}
- */

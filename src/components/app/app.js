1a240e82ad181d5b8b9081fcd1f13eb85e63c8a1
artem <ip.bakhmatov@mail.ru>
2023-01-20 19:27:55 +0300

    Свойства компонентов

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -23,3 +23,34 @@ function App() {
 }
 
 export default App;
+
+////////// Cвойства компонентов //////////////
+/* function WhoAmI (props) { 
+	return (
+		<div>
+			<h1>My name is {props.name}, surname - {props.surname}</h1>
+			<a href={props.link}>My Profile</a>
+		</div>
+	)
+}
+
+function WhoAmI ({name, surname, link}) {  // через деструктирезацию
+	return (
+		<div>
+			<h1>My name is {name()}, surname - {surname}</h1>
+			<a href={link}>My Profile</a>
+		</div>
+	)
+}
+
+// Пропсы (props) -> свойтво по русски
+
+function App() {
+	return (
+		<div className="App">
+			<WhoAmI name={() => {return 'John'}} surname="Smith" link="facebook.com"/>
+			<WhoAmI name="Alex" surname="Shepard" link="vk.com"/>
+		</div>
+	)
+}
+ */

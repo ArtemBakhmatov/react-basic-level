785df4b950f8f6462cd60f1ed4c5f7e6f69f3acf
artem <ip.bakhmatov@mail.ru>
2023-01-24 23:04:17 +0300

    Событие клика на сотрудника -> появляется или изчезает звездочка

--- src/components/employees-list-item/employees-list-item.js
+++ src/components/employees-list-item/employees-list-item.js
@@ -5,7 +5,8 @@ class EmployeesListItem  extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            increase: false
+            increase: false,
+            rise: false
         }
     }
 
@@ -14,19 +15,31 @@ class EmployeesListItem  extends Component {
             increase: !increase
         }))
     }
+
+    onRise = () => {
+        this.setState(({rise}) => ({
+            rise: !rise
+        }))
+    }
     // после => прописываем в скобках , чтобы не писать return
 
     render() {
         const {name, salary} = this.props;
-        const {increase} = this.state;
-        let classNames = 'list-group-item d-flex justify-content-between like';
+        const {increase, rise} = this.state;
+
+        let classNames = 'list-group-item d-flex justify-content-between';
+
         if (increase) {
             classNames += ' increase';
         }
 
+        if (rise) {
+            classNames += ' like';
+        }
+
         return (
             <li className={classNames}>
-                <span className="list-group-item-label">{name}</span>
+                <span className="list-group-item-label" onClick={this.onRise}>{name}</span>
                 <input type="text" className="list-group-item-input" defaultValue={salary + '$'}/>
                 <div className='d-flex justify-content-center align-items-center'>
                     <button type="button"

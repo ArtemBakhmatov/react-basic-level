cfe44913b9800a1259668a38e2111bfb8a572c7b
artem <ip.bakhmatov@mail.ru>
2023-01-28 16:49:23 +0300

    Стили в React. Inline Styles

--- src/components/employees-list-item/employees-list-item.js
+++ src/components/employees-list-item/employees-list-item.js
@@ -2,6 +2,10 @@ import './employees-list-item.css';
 
 const EmployeesListItem = (props) => {
     const {name, salary, onDelete, onToggleProp, increase, rise} = props;
+    /* const styleSpans = {
+        fontSize: '40px',
+        color: 'red'
+    } */
 
     let classNames = 'list-group-item d-flex justify-content-between';
 
@@ -15,7 +19,10 @@ const EmployeesListItem = (props) => {
 
     return (
         <li className={classNames}>
-            <span className="list-group-item-label" onClick={onToggleProp} data-toggle="rise">{name}</span>
+            <span className="list-group-item-label" 
+                  onClick={onToggleProp} 
+                  data-toggle="rise"
+                  /* style={styleSpans} */>{name}</span>
             <input type="text" className="list-group-item-input" defaultValue={salary + '$'}/>
             <div className='d-flex justify-content-center align-items-center'>
                 <button type="button"
